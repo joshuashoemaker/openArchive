edca70eaec68adef23982c2f06acda2c50781bba
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-10-01 21:07:39 -0500

    🎁 feat: add file menu on app start

--- src/Views/App.tsx
+++ src/Views/App.tsx
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
 import AddFileView from './AddFile/AddFileView'
-import './App.css'
 import DocumentEditorView from './DocumentEditor/DocumentEditorView'
 import FileTree from './FileTree/FileTree'
 import Menu from './Menu/Menu'
 import TextEditorView from './TextEditor/TextEditorView'
 import TopNavigation from './TopNavigation/TopNavigation'
 
+import DocumentFileCollection from '../Entities/Collections/DocumentFileCollection' 
+
+import './App.css'
 
 type Props = {}
 type State = { isAddFileOpen: boolean } 
 
 class App extends Component<Props, State> {
+  documentFileCollection = new DocumentFileCollection ()
   constructor () {
     super({})
     this.state = { isAddFileOpen: false }
     document.addEventListener('toggleAddFile', () => { this.toggleAddFile() })
+  }
+
+  componentDidMount = () => {
+    if (this.documentFileCollection.elements.length <1 ) this.setState({ isAddFileOpen: true })
   }
 
   toggleAddFile = () => {
