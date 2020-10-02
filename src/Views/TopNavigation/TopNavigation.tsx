928f38c255a6f337240241cb0326a893e17d14aa
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-10-01 20:22:14 -0500

    🎁 feat: add image from top nav icon

--- src/Views/TopNavigation/TopNavigation.tsx
+++ src/Views/TopNavigation/TopNavigation.tsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import './TopNavigation.css'
 
-import menuIcon from '../../media/icons/barMenu.svg'
+import menuIcon from '../../media/icons/addImageIcon.svg'
 import textViewIcon from '../../media/icons/editFileicon.svg'
 
 class TopNavigation extends Component {
-  toggleMenuEvent: Event
+  toggleAddFile: Event
   toggleTextEditorOpenEvent: Event
   constructor () {
     super({})
-    this.toggleMenuEvent = new Event('toggleMenu')
+    this.toggleAddFile = new Event('toggleAddFile')
     this.toggleTextEditorOpenEvent = new Event('toggleTextEditor')
   }
 
-  handleMenuIconClick = () => {
-    document.dispatchEvent(this.toggleMenuEvent)
+  handleAddFileIconClick = () => {
+    document.dispatchEvent(this.toggleAddFile)
   }
 
   handleTextEditorIconClick = () => {
@@ -24,7 +24,7 @@ class TopNavigation extends Component {
   render = () => {
     return (
       <nav className="TopNavigation">
-        <img id='mainNavIcon' className='menuIcon' src={menuIcon} alt='menu icon' onClick={this.handleMenuIconClick} />
+        <img id='mainNavIcon' className='menuIcon' src={menuIcon} alt='menu icon' onClick={this.handleAddFileIconClick} />
         <h1 id='navigationAppName' className='brandColor'>Choice Archive</h1>
         <img id='mainNavIcon' className='menuIcon' src={textViewIcon} alt='view text icon' onClick={this.handleTextEditorIconClick} />
       </nav>
